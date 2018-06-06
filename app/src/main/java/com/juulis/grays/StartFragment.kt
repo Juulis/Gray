7720bac57a3a60c857cbf844d92bbfaad897eae5
Juulis <danny.p@live.se>
2018-06-06 20:37:54 +0200

    fixed togglebutton alert snackbar

--- app/src/main/java/com/juulis/grays/StartFragment.kt
+++ app/src/main/java/com/juulis/grays/StartFragment.kt
@@ -3,7 +3,6 @@ package com.juulis.grays
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +10,14 @@ import android.widget.LinearLayout
 import android.widget.ToggleButton
 import kotlinx.android.synthetic.main.fragment_start.*
 import java.util.*
+import android.support.design.widget.Snackbar
+
 
 class StartFragment : Fragment(), View.OnClickListener {
 
     private lateinit var iMainActivity: IMainActivity
     private val TAG = "Testing in StartFragment "
+    private var doseButtons: MutableList<ToggleButton> = mutableListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_start, container, false)
@@ -35,7 +37,7 @@ class StartFragment : Fragment(), View.OnClickListener {
         super.onStart()
         val startDate = iMainActivity.getStoredDate()
         val medication = Medication(Date(startDate), Date())
-        val doseButtonContainers: MutableList<LinearLayout> = mutableListOf(toggle_button_1,toggle_button_2,toggle_button_3,toggle_button_4)
+        val doseButtonContainers: MutableList<LinearLayout> = mutableListOf(toggle_button_1, toggle_button_2, toggle_button_3, toggle_button_4)
         start_title.text = getString(R.string.start_title)
         if (medication.fase != null) {
             start_bulk_amount.text = getString(R.string.start_bulk_amount, medication.bulkAmount)
@@ -49,12 +51,11 @@ class StartFragment : Fragment(), View.OnClickListener {
         setupToggleButtons()
 
 
-
     }
 
     private fun setupToggleButtons() {
         val checkBoxList = iMainActivity.getStartToggleButtons()
-        val doseButtons: MutableList<ToggleButton> = mutableListOf(start_first_dose_button, start_second_dose_button, start_third_dose_button, start_fourth_dose_button)
+        doseButtons = mutableListOf(start_first_dose_button, start_second_dose_button, start_third_dose_button, start_fourth_dose_button)
 
         doseButtons.forEach { btn ->
             if (checkBoxList[doseButtons.indexOf(btn)]) {
@@ -69,11 +70,26 @@ class StartFragment : Fragment(), View.OnClickListener {
         when (v!!.id) {
             start_calendar_button.id -> iMainActivity.inflateFragment(getString(R.string.calendar_fragment))
             start_settings_button.id -> iMainActivity.inflateFragment(getString(R.string.settings_fragment))
-            start_first_dose_button.id -> iMainActivity.setStartToggleButtons(0, start_first_dose_button.isChecked)
-            start_second_dose_button.id -> iMainActivity.setStartToggleButtons(1, start_second_dose_button.isChecked)
-            start_third_dose_button.id -> iMainActivity.setStartToggleButtons(2, start_third_dose_button.isChecked)
-            start_fourth_dose_button.id -> iMainActivity.setStartToggleButtons(3, start_fourth_dose_button.isChecked)
+            start_first_dose_button.id -> toggleButton(0)
+            start_second_dose_button.id -> toggleButton(1)
+            start_third_dose_button.id -> toggleButton(2)
+            start_fourth_dose_button.id -> toggleButton(3)
+        }
+    }
 
+    private fun toggleButton(i: Int) {
+        if (!doseButtons[i].isChecked) {
+            val mySnackbar = Snackbar.make((start_fragment_layout), getString(R.string.toggle_state_untoggled), Snackbar.LENGTH_LONG)
+            mySnackbar.setAction(R.string.undo_string, MyUndoListener(i))
+            mySnackbar.show()
+            iMainActivity.setStartToggleButtons(i,false)
+        }else iMainActivity.setStartToggleButtons(i, true)
+    }
+
+    internal inner class MyUndoListener(val i: Int) : View.OnClickListener {
+        override fun onClick(v: View) {
+            doseButtons[i].toggle()
+            iMainActivity.setStartToggleButtons(i,true)
         }
     }
 
